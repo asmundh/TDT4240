1ce98d22f0447e9be7c0e075fe206181a54abba0
AsmundH <aasmund.haugse@gmail.com>
2019-03-18 10:42:09 +0100

    Added comments to components

--- CHAD/core/src/com/mygdx/game/components/CardPowerComponent.java
+++ CHAD/core/src/com/mygdx/game/components/CardPowerComponent.java
@@ -1,5 +1,12 @@
 package com.mygdx.game.components;
 
+/*
+ * This component describes the power of a card.
+ * A power has a name and a text describing it.
+ * It also has a size, e.g. +4,
+ * and a type, e.g. damage.
+ */
+
 public class CardPowerComponent {
     public String powerName;
     public String powerEffectText;
--- CHAD/core/src/com/mygdx/game/components/CardStatsComponent.java
+++ CHAD/core/src/com/mygdx/game/components/CardStatsComponent.java
@@ -2,6 +2,12 @@ package com.mygdx.game.components;
 
 import com.badlogic.ashley.core.Component;
 
+/*
+ * Component of the base card. Has a
+ * Power, a given health and a given cost.
+ * Each card also has a special power, here a CardPowerComponent.
+ */
+
 public class CardStatsComponent implements Component {
     public float power;
     public float health;
