e74fdbecfd112d166f33a3f7b3e64aafc674da44
marisofie <marilerfaldet@gmail.com>
2019-03-19 11:20:59 +0100

    Added files from gsm-branch. Changed CardGame

--- CHAD/core/src/com/mygdx/game/CardGame.java
+++ CHAD/core/src/com/mygdx/game/CardGame.java
@@ -1,37 +1,33 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.model.screens.MenuScreen;
-import com.mygdx.game.model.systems.GameStateManager;
 
-public class CardGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-
-	private GameStateManager gsm;
+public class CardGame extends Game {
+	public SpriteBatch batch;
+	private Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		gsm = new GameStateManager();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		gsm.push(new MenuScreen(gsm));
+		this.setScreen(new MenuScreen(this));
+		Engine engine = new Engine();
 	}
 
 	@Override
 	public void render () {
-		//Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
-		/*batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();*/
+		GL20 gl = Gdx.gl;
+		gl.glClearColor(0.5f, 1.0f, 1.0f, 1.0f);
+		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		super.render();
 	}
 	
 	@Override
--- CHAD/core/src/com/mygdx/game/model/screens/MenuScreen.java
+++ CHAD/core/src/com/mygdx/game/model/screens/MenuScreen.java
@@ -1,42 +1,51 @@
 package com.mygdx.game.model.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.model.components.StateComponent;
-import com.mygdx.game.model.systems.GameStateManager;
+import com.mygdx.game.CardGame;
 
-public class MenuScreen extends StateComponent {
+public class MenuScreen extends ScreenAdapter implements ScreenInterface {
     private Texture playBtn;
+    private CardGame game;
 
-    public MenuScreen(GameStateManager gsm) {
-        super(gsm);
-         playBtn = new Texture("badlogic.jpg");
+    public MenuScreen(CardGame game) {
+        playBtn = new Texture("badlogic.jpg");
+        this.game = game;
+
+    }
+
+    @Override
+    public void update(float dt) {
+        System.out.print("Menu");
+        handleInput();
+    }
+
+    @Override
+    public void draw() {
+        GL20 gl = Gdx.gl;
+        gl.glClearColor(1, 0, 0, 1);
+        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.disableBlending();
+        game.batch.begin();
+        game.batch.draw(playBtn, 0, 0, 320, 480);
+        game.batch.end();
+    }
+
+    @Override
+    public void render (float dt) {
+        update(dt);
+        draw();
+
     }
 
     @Override
     public void handleInput() {
         if (Gdx.input.justTouched()) {
-            gsm.set(new GameScreen(gsm));
-            dispose();
+            game.setScreen(new GameScreen(game));
         }
-    }
 
-    @Override
-    public void update(float dt) {
-        handleInput();
-    }
-
-    @Override
-    public void render(SpriteBatch sb) {
-        sb.begin();
-        sb.draw(playBtn, (Gdx.graphics.getWidth()/2) - (playBtn.getWidth() / 2),
-                (Gdx.graphics.getHeight()/2));
-        sb.end();
-    }
-
-    @Override
-    public void dispose() {
-        playBtn.dispose();
     }
 }
