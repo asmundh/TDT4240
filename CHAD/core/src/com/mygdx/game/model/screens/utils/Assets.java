269b1cc2d9c113abd044c480d101769f20a9595f
Jesper Brynildsen <jesper_bry@hotmail.com>
2019-03-24 14:31:35 +0100

    Add settings screen #11

--- CHAD/core/src/com/mygdx/game/model/screens/utils/Assets.java
+++ CHAD/core/src/com/mygdx/game/model/screens/utils/Assets.java
@@ -7,14 +7,13 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 public class Assets {
 
     public static String badlogic = "badlogic.jpg";
-
     public static String playBtn = "textures/playBtn.png";
-
     public static String settingBtn = "textures/settingBtn.png";
-
     public static String background = "textures/background.png";
-
     public static String menuBG = "textures/menu_bg.png";
+    public static String backBtn = "textures/back.png";
+    public static String exitBtn = "textures/exit.png";
+    public static String exit_gameBtn = "textures/exit_game.png";
 
     private static AssetManager assetManager;
 
@@ -32,6 +31,9 @@ public class Assets {
         assetManager.load(settingBtn, Texture.class);
         assetManager.load(background, Texture.class);
         assetManager.load(menuBG, Texture.class);
+        assetManager.load(backBtn, Texture.class);
+        assetManager.load(exitBtn, Texture.class);
+        assetManager.load(exit_gameBtn, Texture.class);
     }
 
     public static float getProgress() {
