532e7f5b9ed6b1a63d920fe2c7b2af68877da893
AsmundH <aasmund.haugse@gmail.com>
2019-03-25 11:42:44 +0100

    Added card assets and two default decks

--- CHAD/core/src/com/mygdx/game/model/screens/utils/Assets.java
+++ CHAD/core/src/com/mygdx/game/model/screens/utils/Assets.java
@@ -3,6 +3,9 @@ package com.mygdx.game.model.screens.utils;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import java.util.HashMap;
 
 public class Assets {
 
@@ -13,6 +16,22 @@ public class Assets {
     public static String settingBtn = "textures/settingBtn.png";
 
     public static String background = "textures/background.png";
+
+    public static String goblin = "textures/background.png";
+
+    public static String orc = "textures/cards/goblin.png";
+
+    public static String goblin_king = "textures/background.png";
+
+    public static HashMap<String, String> cards = new HashMap<String, String>() {{
+        put("goblin", "textures/cards/goblin.png");
+        put("orc", "textures/cards/orc.png");
+        put("ogre", "textures/cards/ogre.png");
+        put("magma_golem", "textures/cards/magma_golem.png");
+        put("ice_golem", "textures/cards/ice_golem.png");
+        put("moss_golem", "textures/cards/moss_golem.png");
+    }};
+
 
     private static AssetManager assetManager;
 
@@ -29,6 +48,10 @@ public class Assets {
         assetManager.load(playBtn, Texture.class);
         assetManager.load(settingBtn, Texture.class);
         assetManager.load(background, Texture.class);
+
+        for (String card : cards.keySet()) {
+            assetManager.load(cards.get(card), Texture.class);
+        }
     }
 
     public static float getProgress() {
