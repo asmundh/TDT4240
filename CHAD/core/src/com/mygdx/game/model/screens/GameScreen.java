d37d641f4bc5877fb4749b045c4858587d6d5c91
unknown <mkse1995@gmail.com>
2019-03-26 12:19:00 +0100

    Made cardView and boardView.

--- CHAD/core/src/com/mygdx/game/model/screens/GameScreen.java
+++ CHAD/core/src/com/mygdx/game/model/screens/GameScreen.java
@@ -3,14 +3,45 @@ package com.mygdx.game.model.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.CardGame;
+import com.mygdx.game.view.BoardView;
+import com.mygdx.game.view.CardView;
 
 public class GameScreen extends ScreenAdapter implements ScreenInterface {
 
     private CardGame game;
 
+    private BoardView bv;
+    private SpriteBatch batch;
+    private CardView cv1 = new CardView("goblin.png", 3, 5);
+
+
+
     protected GameScreen(CardGame game) {
         this.game = game;
+        this.bv = new BoardView(game);
+        this.batch = new SpriteBatch();
+
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+        this.bv.addCardToBoard(cv1);
+
+
+        this.bv.addCardToHand(cv1);
+        this.bv.addCardToHand(cv1);
+        this.bv.addCardToHand(cv1);
+        this.bv.addCardToHand(cv1);
+        this.bv.addCardToHand(cv1);
+
+        this.bv.setShowHand(false);
+
     }
 
     @Override
@@ -23,9 +54,9 @@ public class GameScreen extends ScreenAdapter implements ScreenInterface {
 
     @Override
     public void draw() {
-        GL20 gl = Gdx.gl;
-        gl.glClearColor(0, 0, 1, 1);
-        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        bv.draw(batch);
 
     }
 
