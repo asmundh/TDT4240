861b7d367b2897b6ef44e2d12617f4a556881a36
Jesper Brynildsen <jesper_bry@hotmail.com>
2019-03-28 10:15:10 +0100

    Add playerSystem #36

--- CHAD/core/src/com/mygdx/game/model/systems/PlayerSystem.java
+++ CHAD/core/src/com/mygdx/game/model/systems/PlayerSystem.java
@@ -1,10 +1,36 @@
 package com.mygdx.game.model.systems;
 
-public class PlayerSystem {
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.mygdx.game.model.components.PlayerComponent;
 
-    public PlayerSystem() {
-        /*
-        TODO: update functionality for players.
-         */
+public class PlayerSystem extends IteratingSystem {
+    private ComponentMapper<PlayerComponent> pl = ComponentMapper.getFor(PlayerComponent.class);
+    private PlayerComponent player = new PlayerComponent();
+
+
+    public PlayerSystem(Family family) {
+        super(family.all(PlayerComponent.class).get());
+    }
+
+    // Take last card from deck, and add to hand list
+    public void pickFromDeck() {
+        player.hand.add(player.deck.get(player.deck.size() - 1));
+    }
+
+    // From hand to table
+    public void AddCardToTable(int index) {
+        player.cardsOnTable.add(player.hand.get(index));
+    }
+
+    public void removeCardOnTable(int index) {
+        player.cardsOnTable.remove(index);
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        PlayerComponent player = pl.get(entity);
     }
 }
