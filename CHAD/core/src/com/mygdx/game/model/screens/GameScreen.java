722235d0afb536ba2736b1ce9a4b7b3a5306bae6
Åsmund Haugse <33457035+asmundh@users.noreply.github.com>
2019-05-04 16:28:33 +0200

    Update GameScreen.java

--- CHAD/core/src/com/mygdx/game/model/screens/GameScreen.java
+++ CHAD/core/src/com/mygdx/game/model/screens/GameScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
